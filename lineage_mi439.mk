c472a47a6ae568f76a244e12baf55aa7b7453c54
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-06 08:50:38 +0600

    Being up

--- lineage_mi439.mk
+++ lineage_mi439.mk
@@ -20,15 +20,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mi439/device.mk)
 
 # Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_mi439
+PRODUCT_NAME := lineage_mi439
 PRODUCT_DEVICE := mi439
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Boot animation resolution.
+
+# TenX Flags
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
